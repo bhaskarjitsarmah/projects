b78923b2304449d095ebc796abdde09375e8a3f1
Bhaskarjit Sarmah <bhaskarjitsarmah@gmail.com>
2017-09-23 15:08:22 +0530

    Added Post Hoc Analysis

--- Data Analysis and Interpretation/Course 2- Data Analysis Tools/Week 1/anova_test.py
+++ Data Analysis and Interpretation/Course 2- Data Analysis Tools/Week 1/anova_test.py
@@ -3,9 +3,9 @@
 
 # ## Research Question
 
-# Is there any relationship between variables **diet** taken and **weight loss** by a respondent? I have taken the dataset **diet_exercise** to perform this test.
+# The research question I am trying to answer in this assignment is **Is there any relationship between variables mpg (miles per gallon) of a car and cylinders (number of cylinders) the car have?** I have taken the dataset [auto-mpg dataset](http://archive.ics.uci.edu/ml/datasets/Auto+MPG) from UCI Machine Learning Repository.
 
-# The dataset consist of two explanatory variables **Exercise** and **Diet** (Categorical in nature) and the response variable **WeightLoss**. Since the explanatory variable is categorical in nature and **WeightLoss** variable is numerical in nature, we need to perform the **ANOVA** test to check for any relationship between these two variables. And we can ignore the variable **Exercise** as of now as we are only interested in knowing the relationship between the variables **Diet** and **WeightLoss**.
+# The variable **mpg** is my response variable which is continuous quantitative variable and the **cylinders** is the explanatory variable which is a categorical variable. So to test the relationship between these two variables, I need to perform the ANOVA Test.
 
 # ## Loading libraries
 
@@ -21,8 +21,11 @@ import statsmodels.stats.multicomp as multi
 
 # In[2]:
 
-data = pd.read_csv("diet_exercise.csv")
+data = pd.read_csv("auto-mpg.data", delim_whitespace=True, header=None, usecols=[0,1], names=['mpg', 'cylinders'], 
+                   dtype={'mpg':np.float64, 'cylinders':'category'})
 
+
+# ## Summarizing the data
 
 # In[3]:
 
@@ -36,18 +39,28 @@ data.head(n=3)
 
 # In[5]:
 
+data.info()
+
+
+# In[6]:
+
+data.describe()
+
+
+# In[7]:
+
 # removing the unnecessary variable
-data = data.drop('Exercise', axis=1)
+data['cylinders'].value_counts()
 
 
 # ## Setting Hypothesis
 
-# As we are testing the relationship between the variables **Diet** and **WeightLoss**, we can explain the null hypothesis as follows-
+# As we are testing the relationship between the variables **mpg** and **cylinders**, we can explain the null hypothesis as follows-
 
 # <ol><li>**Null Hypothesis:** There is nothing going on between the variables, there is no relationship between the two variables, in mathematical terms \begin{align}
-# \\H_0: \mu_A & = \mu_B 
+# \\H_0: \mu_3 = \mu_4 = \mu_5 = \mu_6 = \mu_8 
 # \end{align}</li><br><li>**Alternate Hypothesis:** There is something going on between the explanatory and response variable, or there is a relationship between the two, in mathematical terms \begin{align}
-# \\H_A: \mu_A & \neq \mu_B 
+# \\H_A: \mu_3 \neq \mu_4 \neq \mu_5 \neq \mu_6 \neq \mu_8
 # \end{align}</li></ol>
 
 # ## Visualizing data
@@ -57,18 +70,33 @@ data = data.drop('Exercise', axis=1)
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.boxplot(x=data['Diet'], y=data['WeightLoss'])
+sns.boxplot(x=data['cylinders'], y=data['mpg'], showmeans=True)
 plt.show()
 
 
-# From the above plot it looks like the mean of these two groups of observations (Diet A and Diet B) are different from each other. It signifies the distributions for these two Diet categories are from two independent populations and there is a relationship between the variable **Diet** and **WeightLoss**. But we need to prove this visual inference through a statistical test.
+# From the above plot, it is clearly visible that the mean (triangular shape in red color) of the group with 8 number of cylinders does not overlap with another group means. We can now say that we have evidence against the null hypothesis and the variables are related to each other. But lets prove this by performing the ANOVA Test.
 
-# In[19]:
+# ## ANOVA F Test
+
+# In[9]:
 
 # using ols function for calculating the F-statistic and associated p value
-model1 = smf.ols(formula='WeightLoss ~ C(Diet)', data=data)
-results1 = model1.fit()
-print (results1.summary())
+model = smf.ols(formula='mpg ~ cylinders', data=data)
+results = model.fit()
+print (results.summary())
 
 
-# From the statistical results above we get **p-value = 0.00133** which is less than the standard significance level 0.05. Therefore we can reject our null hypothesis as the data shows significant evidence that probability of finding the observed or more extreme results provided the null hypothesis is true is very very less. So the relationship between the variable **Diet** and **WeightLoss** is statistically significant.
+# As we can observe from the above table, the F-statistic is very high at 172.6 with the very very low p-value. So, we can reject our null hypothesis and conclude that there is a relationship between the categorical predictor variable cylinder (number of cylinders in the car) and quantitative target variable mpg (mileage of the car).
+
+# ## Post Hoc Analysis
+
+# Now we have evidence against the null hypothesis and proved that there is a relationship between number of cylinders and miles per gallon of a car, we need to check which which number of cylinders effect mileage of car the most. In other words, mpg for which number of cylinders are significantly different from each other. To do this, we need to perform post-hoc analysis of the above ANOVA Test.
+
+# In[10]:
+
+mc1 = multi.MultiComparison(data['mpg'], data['cylinders'])
+res1 = mc1.tukeyhsd()
+print(res1.summary())
+
+
+# From the above result, it is evident that the cars with 4 and 8 number of cylinders differs significantly in mileage (mpg) when compares to cars with 3, 5 and 6 cylinders.
