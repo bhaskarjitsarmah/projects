107ee9b017d9b54b82c7fee6a89fa1957e1821f1
bhaskarjitsarmah <bhaskarjitsarmah@gmail.com>
2019-06-13 11:42:33 +0530

    recfun assignment initial commit

---
+++ Functional Programming in Scala/Course 1 - Functional Programming Principles in Scala/Week 1/recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,41 @@
+package recfun
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+    def pascal(c: Int, r: Int): Int = {
+      if(c==0 || c==r) 1
+      else pascal(c-1, r-1) + pascal(c, r-1)
+    }
+
+  /**
+   * Exercise 2
+   */
+    def balance(chars: List[Char]): Boolean = {
+      def isParanthesesBalanced(chars: List[Char], open: Int): Boolean =
+        if (chars.isEmpty) open == 0
+        else if (chars.head == '(') isParanthesesBalanced(chars.tail, open + 1)
+        else if (chars.head == ')') open > 0 && isParanthesesBalanced(chars.tail, open - 1)
+        else isParanthesesBalanced(chars.tail, open)
+      isParanthesesBalanced(chars, 0)
+    }
+
+  /**
+   * Exercise 3
+   */
+    def countChange(money: Int, coins: List[Int]): Int = {
+      if (money == 0) 1
+      else if (money > 0 && !coins.isEmpty) countChange(money - coins.head, coins) + countChange(money, coins.tail)
+      else 0
+    }
+  }
